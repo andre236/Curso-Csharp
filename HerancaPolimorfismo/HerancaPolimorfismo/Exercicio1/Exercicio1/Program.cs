17117604b2d539d25d1965bfa84639e6f6d77ee0
André Arruda da Silva <adr_hb@hotmail.com>
2021-07-22 08:52:41 -0300

    Exercicio com acompanhamento.

---
+++ HerancaPolimorfismo/HerancaPolimorfismo/Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Exercicio1.Entities;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Enter the number of employees: ");
+            int numberEmployees = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numberEmployees; i++)
+            {
+                Console.WriteLine($"Employee #{i} data:");
+                Console.Write("Outsourced (y/n)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour? ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if(ch == 'y')
+                {
+                    Console.Write("Additional Charger: ");
+                    double additionalCharger = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharger));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("PAYMENTS: ");
+                foreach(Employee emp in list)
+                {
+                    Console.WriteLine(emp.Name + " - $" + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+                }
+
+            }
+        }
+    }
+}
