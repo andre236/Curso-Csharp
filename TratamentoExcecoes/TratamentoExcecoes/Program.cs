b2dae67c82f1a48e70510925d10b323a0c3ff8fc
André Arruda da Silva <adr_hb@hotmail.com>
2022-03-17 21:19:36 -0300

    Criando tratamento de excecoes

---
+++ TratamentoExcecoes/TratamentoExcecoes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TratamentoExcecoes.Entities;
+using TratamentoExcecoes.Entities.Exceptions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Room Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            Console.WriteLine("Reservation: " + reservation);
+            Console.WriteLine();
+
+            Console.WriteLine("Check-in date (dd/MM/yyyy): ");
+            checkIn = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Check-out date (dd/MM/yyyy): ");
+            checkOut = DateTime.Parse(Console.ReadLine());
+
+            reservation.UpdateDates(checkIn, checkOut);
+            Console.WriteLine("Reservation: "+reservation);
+        }
+        catch (DomainException e)
+        {
+            Console.WriteLine("Erro na reserva: " + e.Message);
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine("Erro a formatacao: " + e.Message);
+        }
+ 
+
+
+    }
+}
